bdcadbfb460cd851968fd384e67383e79ef9d0a3
BrainAxe <tanzimrizwan@gmail.com>
2016-12-24 01:23:42 +0600

    changed to simple

--- dir_list.py
+++ dir_list.py
@@ -1,20 +1,10 @@
 import os
 
-c_dir = os.getcwd()
-file = open("list.txt",'w')
-d = raw_input('Enter the directory address: ')
-i=1
-for f in os.listdir(d):
-	if os.path.isdir(os.path.join(d, f)):
-		s1 = str(i) + ". " + f + "\n"
-		file.write(s1)
-		f1 = d + "/" + f
-		for f2 in os.listdir(f1):
-			s1 = "\t *" + f2 + "\n"
-			file.write(s1)
-	else:
-		s = str(i) + ". " + f + "\n"
-		file.write(s)
-	i+=1
-os.chdir(c_dir)
-file.close()
+directory = raw_input('Enter the directory location: ')
+i = 1
+
+with open("list.txt", 'w') as out:
+	for item in os.listdir(directory):
+		items = "{:2}. {}\n".format(i, item)
+		out.write(items)
+		i+=1
