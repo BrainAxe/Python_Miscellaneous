a63a3c3958a3bd2e989239f5e88de83ff1ba1ab2
BrainAxe <tanzimrizwan@gmail.com>
2017-09-30 01:19:13 +0600

    adding get_browser_history script

---
+++ get_browser_history.py
@@ -0,0 +1,102 @@
+import os 
+import sqlite3
+import operator
+from collections import OrderedDict
+import matplotlib.pyplot as plt 
+
+
+def parse(url):
+	try:
+		parsed_url_components = url.split('//')
+		sublevel_split = parsed_url_components[1].split('/',1)
+		domain = sublevel_split[0].replace("www.", "")
+		return domain 
+
+	except IndexError:
+		pass
+		#print "URL format error!"
+
+
+def analyze(results):
+	prompt = raw_input("[.] Type <c> to print or <p> to plot\n [>] ")
+
+	if prompt == "c":
+		for site, count in sites_count_sorted.items():
+			print site, count
+
+	elif prompt == "p":
+		# plt.bar(range(len(results)), results.values(), align='edge')
+		results2 = {}
+		for i in results:
+			if results[i]>200:
+				results2[i] = results[i]
+
+		plt.bar(range(len(results2)), results2.values(), align='edge')
+		plt.xticks(rotation=45)
+		plt.xticks(range(len(results2)), results2.keys())
+		plt.show()	
+		
+	else:
+		print "[.] Uh?"
+		quit()	
+
+
+def main():
+
+	print "Choose Web Browser:"
+	print "1. Firefox"
+	print "2. Chromium"
+	print "3. Google Chrome"
+	print "4. Exit"
+	choice = int(raw_input("Enter your choice: "))
+
+	if choice == 1:
+		history_db = os.path.expanduser('~') + "/.mozilla/firefox/gb537nn3.default-1478607779150/places.sqlite"
+		
+		sql = "SELECT url, visit_count FROM moz_places;"
+	
+
+	elif choice == 2:
+		history_db = os.path.expanduser('~') + "/.config/chromium/Profile 2/History"
+		sql = "SELECT urls.url, urls.visit_count FROM urls, visits WHERE urls.id = visits.url;"
+
+	elif choice == 3:
+		history_db = os.path.expanduser('~') + "/.config/google-chrome/Profile 1/History"
+		sql = "SELECT urls.url, urls.visit_count FROM urls, visits WHERE urls.id = visits.url;"
+
+	elif choice == 4:
+		quit()
+	
+	else:
+		print "Wrong Input!!"
+
+
+
+	con = sqlite3.connect(history_db)
+	con = con.cursor()
+	con.execute(sql)
+	results = con.fetchall()
+
+	sites_count = {}
+
+	for url, count in results:
+		url = parse(url)
+		if url in sites_count:
+			sites_count[url] += 1
+
+		else:
+			sites_count[url] = 1
+
+	global sites_count_sorted
+	
+	sites_count_sorted = OrderedDict(sorted(sites_count.items(), key=operator.itemgetter(1), reverse=True))
+
+	analyze(sites_count_sorted)
+
+
+
+
+
+
+if __name__ == '__main__':
+	main()
