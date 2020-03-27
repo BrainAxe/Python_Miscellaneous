791de67e97c296d176d2d5d4ac01ce88557ec1a3
Tanzim Rizwan <tanzimrizwan@outlook.com>
2020-03-27 15:28:29 +0600

    updated to python 3

--- natWall.py
+++ natWall.py
@@ -1,12 +1,12 @@
 import requests
 from lxml.html import fromstring
 from lxml import etree
-from datetime import datetime 
+from datetime import datetime
 
 
 def download_national_geographic_wallpaper():
     url = 'http://www.nationalgeographic.com/photography/photo-of-the-day/'
-    
+
     r = requests.get(url)
     if r.status_code == 200:
         doc = fromstring(r.text)
@@ -14,9 +14,9 @@ def download_national_geographic_wallpaper():
             prop = meta.get('property')
             if prop == 'og:image':
                 image_url = meta.get('content')
-                print image_url
+                print(image_url)
                 r = requests.get(image_url, stream=True)
-                print r.status_code
+                print(r.status_code)
                 now = datetime.now()
                 fname = 'image-' + str(now) + ".jpg"
                 if r.status_code == 200:
@@ -24,10 +24,10 @@ def download_national_geographic_wallpaper():
                         with open(fname, 'wb') as f:
                             for chunk in r.iter_content(1024):
                                 f.write(chunk)
-                        print "-----Download Complete-------"
-                        
+                        print("-----Download Complete-------")
+
                     except Exception as e:
-                        print e
+                        print(e)
 
 
 if __name__=="__main__":
