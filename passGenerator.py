9b4d9534306320c6e21d1327c904101966560e62
Tanzim Rizwan <tanzimrizwan@outlook.com>
2020-03-27 15:32:06 +0600

    updated to python 3

--- passGenerator.py
+++ passGenerator.py
@@ -1,7 +1,7 @@
 """Script to generate password"""
 
 import sys
-from random import *
+import random
 
 
 def main():
@@ -10,9 +10,9 @@ def main():
     intNum = "0123456789"
     other = "!#$&*@"
     str1 = charUpper + charLower + intNum + other
-    passDigit = int(raw_input("Enter no. of char/digit of your password: "))
-    password = "".join(choice(str1) for i in range(passDigit))
-    print "Your password is generated correctly.\n Your password is: ",password
+    passDigit = int(input("Enter no. of char/digit of your password: "))
+    password = "".join(random.choice(str1) for i in range(passDigit))
+    print("Your password is generated correctly.\nYour password is: ",password)
 
 if __name__ == '__main__':
-    sys.exit(main()) 
+    sys.exit(main())
